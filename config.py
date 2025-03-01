9c3a8805e2c09ec3cb7ad235689ed5484c8846b5
yukeshwarp <Yukeshwarp@docu3c.com>
2025-03-01 21:39:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from openai import AzureOpenAI
+
+# Azure app registration details
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TENANT_ID = os.getenv("TENANT_ID")
+AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
+SCOPE = ["https://graph.microsoft.com/.default"]
+
+# LLM setup
+client = AzureOpenAI(
+    azure_endpoint=os.getenv("LLM_ENDPOINT"),
+    api_key=os.getenv("LLM_KEY"),
+    api_version="2024-10-01-preview",
+)
